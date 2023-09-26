ab49765c274a5d32bef7b2258ca13ab124a40dc5
Carlos Cardona <c.cardona@opportunitynetwork.com>
2023-09-26 09:28:13 +0200

    Add dist static files

---
+++ client/src/components/PillSelect.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-menu>
+    <template v-slot:activator="{ on }">
+      <v-chip variant="outlined" v-on="on">
+        Testing
+        <v-icon icon="mdi-chevron-down" />
+      </v-chip>
+    </template>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'PillSelect'
+}
+</script>
+
+<style scoped></style>
