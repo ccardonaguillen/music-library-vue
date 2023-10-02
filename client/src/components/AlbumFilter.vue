e5d7f8ed8beb0aa608ea723a0443c57c2c42cdec
Carlos Cardona <c.cardona@opportunitynetwork.com>
2023-10-02 15:51:14 +0200

    Add responsiveness to album filters

--- client/src/components/AlbumFilter.vue
+++ client/src/components/AlbumFilter.vue
@@ -3,54 +3,61 @@
     {{ $tc('controls.entries', albumCount, { displayed: shownAlbumCount, total: albumCount }) }}
   </p>
   <h3>{{ $t('controls.filters') }}</h3>
-  <div class="d-flex align-end" style="gap: 32px">
-    <div style="flex: 3">
-      <p class="pl-3">{{ $t('fields.artist') }}</p>
-      <v-autocomplete
-        :items="artists"
-        v-model="filters.artist"
-        multiple
-        chips
-        hide-details
-        density="compact"
-        variant="outlined"
-        rounded
-        placeholder="No artist selected"
-      />
+  <div class="d-flex justify-space-between align-end">
+    <div class="filter-container mr-4">
+      <div style="grid-area: artist">
+        <p class="pl-3">{{ $t('fields.artist') }}</p>
+        <v-autocomplete
+          :items="artists"
+          v-model="filters.artist"
+          multiple
+          chips
+          hide-details
+          density="compact"
+          variant="outlined"
+          rounded
+          placeholder="No artist selected"
+        />
+      </div>
+
+      <div style="grid-area: owned">
+        <p class="pl-3">{{ $t('fields.owned') }}</p>
+        <v-select
+          v-model="filters.owned"
+          :items="trueFalseOptions"
+          item-value="value"
+          item-title="label"
+          hide-details
+          density="compact"
+          variant="outlined"
+          rounded
+        />
+      </div>
+
+      <div style="grid-area: favorite">
+        <p class="pl-3">{{ $t('fields.favorite') }}</p>
+        <v-select
+          v-model="filters.favorite"
+          :items="trueFalseOptions"
+          item-value="value"
+          item-title="label"
+          hide-details
+          density="compact"
+          variant="outlined"
+          rounded
+        />
+      </div>
     </div>
 
-    <div style="flex: 1">
-      <p class="pl-3">{{ $t('fields.owned') }}</p>
-      <v-select
-        v-model="filters.owned"
-        :items="trueFalseOptions"
-        item-value="value"
-        item-title="label"
-        hide-details
-        density="compact"
-        variant="outlined"
-        rounded
-      />
-    </div>
-
-    <div style="flex: 1">
-      <p class="pl-3">{{ $t('fields.favorite') }}</p>
-      <v-select
-        v-model="filters.favorite"
-        :items="trueFalseOptions"
-        item-value="value"
-        item-title="label"
-        hide-details
-        density="compact"
-        variant="outlined"
-        rounded
-      />
-    </div>
-
-    <v-spacer />
-    <v-btn size="large" prepend-icon="mdi-plus" color="primary" @click="openModal">
-      {{ $t('controls.newAlbum') }}
-    </v-btn>
+    <!--    <v-spacer v-if="$vuetify.display.mdAndUp" />-->
+    <v-btn
+      size="large"
+      prepend-icon="mdi-plus"
+      :text="$t('controls.newAlbum')"
+      color="primary"
+      @click="openModal"
+      v-if="$vuetify.display.mdAndUp"
+    />
   </div>
 </template>
 
@@ -102,4 +109,24 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.filter-container {
+  display: grid;
+  grid-template-areas: 'artist owned favorite';
+  grid-template-rows: auto;
+  grid-template-columns: 3fr 1fr 1fr;
+  gap: 32px;
+}
+
+@media screen and (max-width: 600px) {
+  .filter-container {
+    width: 100%;
+    grid-template-areas:
+      'artist artist'
+      'owned favorite';
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+    gap: 16px;
+  }
+}
+</style>
