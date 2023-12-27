6d68c97c445d93ed12930b267758399a29f93d0c
Carlos Cardona <c.cardona@opportunitynetwork.com>
2023-12-27 20:27:17 +0100

    Add blank album format when loading modal

--- client/src/stores/modal.js
+++ client/src/stores/modal.js
@@ -2,10 +2,34 @@
 
 import { defineStore } from 'pinia'
 
+const blankAlbum = {
+  title: '',
+  artist: '',
+  released: '',
+  genre: '',
+  owned: false,
+  favorite: false,
+  topRS1: null,
+  topRS3: null,
+  wikipedia: '',
+  discogs: '',
+  jacket: '',
+  record_format: [],
+  album_format: null,
+  catalog_num: '',
+  label: '',
+  country: '',
+  edition: null,
+  matrix: '',
+  condition: null,
+  nDisks: null,
+  notes: ''
+}
+
 export const useModalStore = defineStore('modal', {
   state: () => ({
     show: false,
-    album: {}
+    album: blankAlbum
   }),
 
   actions: {
@@ -14,29 +38,7 @@ export const useModalStore = defineStore('modal', {
     },
     closeModal() {
       this.show = false
-      this.album = {
-        title: '',
-        artist: '',
-        released: '',
-        genre: '',
-        owned: false,
-        favorite: false,
-        topRS1: null,
-        topRS3: null,
-        wikipedia: '',
-        discogs: '',
-        jacket: '',
-        record_format: [],
-        album_format: null,
-        catalog_num: '',
-        label: '',
-        country: '',
-        edition: null,
-        matrix: '',
-        condition: null,
-        nDisks: null,
-        notes: ''
-      }
+      this.album = blankAlbum
     }
   }
 })
